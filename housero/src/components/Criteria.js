7ac286cead1c12ebb3c3b982fb68f8ed98774384
DylanMcCarty <dylan.mccarty@student.awesomeinc.org>
2022-12-08 04:52:30 +0000

    fixed small front end criteria adjustments

--- housero/src/components/Criteria.js
+++ housero/src/components/Criteria.js
@@ -1,57 +1,40 @@
 import React from 'react'
-import axios from 'axios'
 import { useGlobalState } from "../context/GlobalState";
 import { useEffect, useState } from 'react';
 import request from '../services/api.request';
 
 
-let startCriteria = {
-  baths: "0",
-  beds: "0",
-  max_price: null,
-  min_price: "0",
-  sqft: "0",
-}
-
 export default function Criteria() {
 
-    const [ state, dispatch ] = useGlobalState();
-    const [data, setData] = useState({
-        baths: "",
-        beds: "",
-        max_price: "",
-        min_price: "",
-        sqft: "",
-    })
+
+  const [rowID, setRowID] = useState()
+  const [ state, dispatch ] = useGlobalState();
+  const [data, setData] = useState({
+      beds: "",
+      baths: "",
+      max_price: "",
+      min_price: "",
+      sqft: "",
+  })
+
  
 
 
 useEffect(() => {
-    async function getCriteria() {
-      let options = {
-        url : `/criteria/`,
-        method : 'GET',
-        params: {
-          'user_id': state.currentUser.user_id,
-        }
+  async function getCriteria() {
+    let options = {
+      url : `/criteria/`,
+      method : 'GET',
+      params: {
+        'user_id': state.currentUser.user_id,
       }
-      let resp = await request(options)
-      setData(resp.data[0])
-      console.log(resp.data)
     }
-
-    getCriteria()
-// //     axios.get(`https://8000-dylanmccart-houseroback-jwpcvpftuv2.ws-us77.gitpod.io/api/criteria/`)
-// //         .then((response) => {
-            
-// //             console.log(response.data)
-// //             setData(response.data);
-
-// //         }, (error) => {
-
-// //         console.log(error);
-
-// //         });
+    let resp = await request(options)
+    setData(resp.data[0])
+    setRowID(resp.data[0].id)
+    console.log(resp.data)
+  }
+  getCriteria()
 }, [])
 
 
@@ -63,10 +46,11 @@ const handleChange = (key, value) => {
     })
 }
 
+
 async function update(e) {
   e.preventDefault()
   let options = {
-    url : `/criteria/?user__id=${state.currentUser.user_id}`,
+    url : `/criteria/${rowID}/`,
     method : 'PATCH',
     data,
   }
@@ -83,29 +67,63 @@ async function update(e) {
     <div>
       <form onSubmit={update} >
       
-        <label>Baths</label>
-        <input 
-        placeholder={data.baths}
-        onChange={(e) => handleChange('baths', e.target.value)}
-        ></input>
-
         <label>Beds</label>
-        <input
-        placeholder={data.beds}
+        <select
+        value={data.beds}
         onChange={(e) => handleChange('beds', e.target.value)}
-        ></input>
+        >
+          <option value={1}>1+</option>
+          <option value={2}>2+</option>
+          <option value={3}>3+</option>
+          <option value={4}>4+</option>
+          <option value={5}>5+</option>
+          <option value={6}>6+</option>
+        </select>
+
+        <label>Baths</label>
+        <select
+        value={data.baths}
+        onChange={(e) => handleChange('baths', e.target.value)}
+        >
+          <option value={1}>1+</option>
+          <option value={2}>2+</option>
+          <option value={3}>3+</option>
+          <option value={4}>4+</option>
+        </select>
 
         <label>max_price</label>
-        <input 
+        <input
+        type={'number'}
         placeholder={data.max_price}
+        onChange={(e) => handleChange('max_price', e.target.value)}>   
+        </input>
+        <select 
         onChange={(e) => handleChange('max_price', e.target.value)}
-        ></input>
+        >
+          <option value={null}>Any</option>
+          <option value={100000}>100,000+</option>
+          <option value={200000}>200,000+</option>
+          <option value={300000}>300,000+</option>
+          <option value={400000}>400,000+</option>
+          <option value={500000}>500,000+</option>
+        </select>
 
         <label>min_price</label>
-        <input 
+        <input
+        type={'number'}
         placeholder={data.min_price}
+        onChange={(e) => handleChange('min_price', e.target.value)}>   
+        </input>
+        <select 
         onChange={(e) => handleChange('min_price', e.target.value)}
-        ></input>
+        >
+          <option value={null}>Any</option>
+          <option value={100000}>100,000</option>
+          <option value={200000}>200,000</option>
+          <option value={300000}>300,000</option>
+          <option value={400000}>400,000</option>
+          <option value={500000}>500,000</option>
+        </select>
 
         <label>sqft</label>
         <input 
--- housero/src/services/auth.constants.js
+++ housero/src/services/auth.constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://8000-dylanmccart-houseroback-jwpcvpftuv2.ws-us77.gitpod.io/api/";
+export const API_URL = "https://8000-dylanmccart-houseroback-jwpcvpftuv2.ws-us78.gitpod.io/api/";
 export const LOGIN_ENDPOINT = "token/obtain/"
 export const REGISTER_ENDPOINT = "user/"
 export const REFRESH_ENDPOINT = "api/"
